cd12c250031482c7a836c8e5366de913614c88bb
Jeffrey Padfield <jefpadfi@gmail.com>
2023-07-16 00:58:27 -0600

    created base gui in kv

---
+++ src/main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.boxlayout import BoxLayout
+
+Builder.load_file('main.kv')
+
+class RootWidget(BoxLayout):
+    pass
+
+class PandaStock(App):    
+    def build(self):
+        return RootWidget()
+
+if __name__ == '__main__':
+    PandaStock().run()
